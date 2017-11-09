0ba7d326ade1cd54d6964a8a18729b2c4c431a47
Jaime D <jdbsull@gmail.com>
2017-11-09 20:33:49 +0000

    Some final fixes

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-
+/* Enum for random cards */
 const CARDS = [
 	{
 		id:"bastos3",
@@ -17,15 +17,17 @@ const CARDS = [
 		path:"img/8decopas.png"
 	}
 ]	
+/* Card Reverse and Gold Ace */
 const REVERSE_PATH="img/back.png";
 const GOLD_ACE_PATH="img/Asdeoros.png";
 const GOLD_ACE_ID="goldace";
 
-
+/* Card Proportions */
 const PROPORTIONAL_FACTOR=0.6518;
 const CARD_HEIGHT=200;
 const CARD_WIDTH=CARD_HEIGHT*PROPORTIONAL_FACTOR;
 
+/* Card definition */
 function Card(config){
 	this.id=config.id||"unknown";
 	this.path=config.path;
@@ -39,10 +41,13 @@ function Card(config){
 }
 
 
-
+/* Constructor */
 Card.prototype.initialize=function(){
+	//Propoerties set
 	this.flop=false;
+	this.animationInterval=0;
 
+	//Card elements
 	this.element = document.createElement("div");
 	this.element.classList.add("card");
 	this.img = document.createElement("img");
@@ -52,8 +57,9 @@ Card.prototype.initialize=function(){
 	this.img.style.height = CARD_HEIGHT + "px";
 	this.element.appendChild(this.img);
 	this.width=CARD_WIDTH;
-	this.animationInterval=0;
 	this.img.id=this.id;
+
+	//Methods
 	this.update();
 }
 
@@ -75,6 +81,7 @@ Card.prototype.preAnimationCycle=function(){
 	this.img.style.width=this.width + "px";
 
 }
+
 Card.prototype.postAnimationCycle=function(){
 	const INCREMENT=9;
 	const LIMIT=CARD_WIDTH;
@@ -133,9 +140,9 @@ Card.prototype.turnDown=function(){
 
 
 
-
-const TURN_DOWN_DELAY_MS=800;
-const FAIL_FLIP_DELAY_MS=500;
+/* Game definition */
+const TURN_DOWN_DELAY_MS=800; //Time for card to flip
+const FAIL_FLIP_DELAY_MS=500; //Time between a fail and Gold Ace flip
 function Game(){
 	this.element;
 	this.counter;
@@ -148,49 +155,7 @@ function Game(){
 }
 
 
-Game.prototype.createRandomCard=function(){
-	var randomCardConfig=0
-	do{
-		randomCardConfig=CARDS[Math.floor(Math.random()*CARDS.length)];
-	}while(this.getCard(randomCardConfig.id));
-	return new Card(randomCardConfig);
-}
-
-Game.prototype.createResetButton=function(){
-	const BUTTON_TEXT="Otra vez";
-	var button=document.createElement("button");
-	button.innerHTML=BUTTON_TEXT;
-	button.addEventListener("click",this.reset.bind(this),true);
-	return button;
-}
-Game.prototype.createHeader=function(){
-	const TITLE="TRILERO";
-	var h2 = document.createElement("h2");
-	h2.innerHTML=TITLE;
-	var header = document.createElement("div");
-	header.classList.add("trilero-header");
-	header.appendChild(h2);
-	return header;
-}
-Game.prototype.createFooter=function(){
-
-	this.resetButton = this.createResetButton();
-	this.resultMessage=document.createElement("h3");
-	this.updateResultMessage();
-	var footer=document.createElement("div");
-	footer.classList.add("trilero-footer");
-	footer.appendChild(this.resultMessage);
-	footer.appendChild(this.resetButton);
-	return footer
-}
-
-Game.prototype.createGoldAceCard=function(){
-	var config = {};
-	config.id=GOLD_ACE_ID;
-	config.path=GOLD_ACE_PATH;
-	return new Card(config);
-}
-
+/* Constructor */
 Game.prototype.initialize=function(){
 	//Attributes
 	this.counter=0;
@@ -219,25 +184,57 @@ Game.prototype.initialize=function(){
 	this.reset();
 }
 
-
-Game.prototype.start=function(){
-	this.shuffle();
-	this.addFlipEvent();
-	if(this.turnDownThread!=0)
-		this.turnDownThread=0;
+/* Interface Definition */
+Game.prototype.createResetButton=function(){
+	const BUTTON_TEXT="Otra vez";
+	var button=document.createElement("button");
+	button.innerHTML=BUTTON_TEXT;
+	button.addEventListener("click",this.reset.bind(this),true);
+	return button;
 }
 
-Game.prototype.shuffle=function(){
-	this.cards.forEach(function(card){
-			var randomInt = parseInt(Math.random()*100);
-			card.getCard().style.order=randomInt.toString();
-		});
+Game.prototype.createHeader=function(){
+	const TITLE="TRILERO";
+	var h2 = document.createElement("h2");
+	h2.innerHTML=TITLE;
+	var header = document.createElement("div");
+	header.classList.add("trilero-header");
+	header.appendChild(h2);
+	return header;
 }
 
-Game.prototype.getCard=function(id){
-	return this.cards.find(element => id==element.id)
+Game.prototype.createFooter=function(){
+
+	this.resetButton = this.createResetButton();
+	this.resultMessage=document.createElement("h3");
+	this.updateResultMessage();
+	var footer=document.createElement("div");
+	footer.classList.add("trilero-footer");
+	footer.appendChild(this.resultMessage);
+	footer.appendChild(this.resetButton);
+	return footer
 }
 
+Game.prototype.createGoldAceCard=function(){
+	var config = {};
+	config.id=GOLD_ACE_ID;
+	config.path=GOLD_ACE_PATH;
+	return new Card(config);
+}
+
+
+
+/* Creates a random card that doesnt exists ingame */
+Game.prototype.createRandomCard=function(){
+	var randomCardConfig=0
+	do{
+		randomCardConfig=CARDS[Math.floor(Math.random()*CARDS.length)];
+	}while(this.getCard(randomCardConfig.id));
+	return new Card(randomCardConfig);
+}
+
+
+/* Flips ace */
 Game.prototype.flipAce=function(event){
 	var card=this.getCard(GOLD_ACE_ID);
 	card.flip();
@@ -245,6 +242,18 @@ Game.prototype.flipAce=function(event){
 	setTimeout(this.buttonEnable.bind(this),TURN_DOWN_DELAY_MS);
 }
 
+
+
+
+/* Button handle */
+Game.prototype.buttonEnable=function(){
+	this.resetButton.removeAttribute("disabled");
+}
+Game.prototype.buttonDisable=function(){
+	this.resetButton.setAttribute("disabled","disabled");
+}
+
+/* Flip event definition */
 Game.prototype.flipEvent=function(event){
 	var target=event.target;
 
@@ -264,13 +273,7 @@ Game.prototype.flipEvent=function(event){
 	}
 }
 
-Game.prototype.buttonEnable=function(){
-	this.resetButton.removeAttribute("disabled");
-}
-Game.prototype.buttonDisable=function(){
-	this.resetButton.setAttribute("disabled","disabled");
-}
-
+/* Event switchs */
 Game.prototype.addFlipEvent=function(){
 	this.element.addEventListener("click", this.boundFlipEvent, true);
 }
@@ -279,6 +282,7 @@ Game.prototype.removeFlipEvent=function(){
 	this.element.removeEventListener("click", this.boundFlipEvent, true);
 }
 
+/* Text Interface */
 Game.prototype.updateResultMessage=function(message){
 	if(message){
 		this.resultMessage.innerHTML=message + "(Has ganado " + this.counter + " veces)";
@@ -288,6 +292,28 @@ Game.prototype.updateResultMessage=function(message){
 	
 }
 
+/* Shuffles cards */
+Game.prototype.shuffle=function(){
+	this.cards.forEach(function(card){
+			var randomInt = parseInt(Math.random()*100);
+			card.getCard().style.order=randomInt.toString();
+		});
+}
+
+/* Returns a Card object by id */
+Game.prototype.getCard=function(id){
+	return this.cards.find(element => id==element.id)
+}
+
+/* Enables game to be playable */
+Game.prototype.start=function(){
+	this.shuffle();
+	this.addFlipEvent();
+	if(this.turnDownThread!=0)
+		this.turnDownThread=0;
+}
+
+/* Restart de game */
 Game.prototype.reset=function(){
 	if(this.turnDownThread==0){
 		this.buttonDisable();
